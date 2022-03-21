3634a528d3e583347a880ee58177fd0fb3c886c3
t-kielkowski <tk.kielkowski@gmail.com>
2022-03-21 19:51:39 +0100

    Added method GetSellerNameList

---
+++ InvoiceGenerator/Infrastructure/Repository/SellerRepository/ISellerRepository.cs
@@ -0,0 +1,12 @@
+﻿using InvoiceGenerator.Infrastructure.Repository.BaseRepository;
+using InvoiceGenerator.InvoiceData;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace InvoiceGenerator.Infrastructure.Repository.SellerRepository
+{
+    public interface ISellerRepository : IBaseRepository<Seller>
+    {
+        Task<ICollection<Seller>> GetSellerNameList();
+    }
+}
---
+++ InvoiceGenerator/Infrastructure/Repository/SellerRepository/SellerRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InvoiceGenerator.InvoiceData;
+using InvoiceGenerator.Infrastructure.Repository.BaseRepository;
+
+namespace InvoiceGenerator.Infrastructure.Repository.SellerRepository
+{
+    public class SellerRepository : BaseRepository<Seller>, ISellerRepository
+    {
+        public SellerRepository(InvocieContex dbContext) : base(dbContext) 
+        { }
+
+        public async Task<ICollection<Seller>> GetSellerNameList()
+        {
+            var sellers = DbContext.Seller
+                .Select(x => new Seller()
+                {
+                    Name = x.Name,
+                    SecondName = x.SecondName
+
+                })
+                .ToList();
+
+            return sellers;
+        }
+    }
+}
