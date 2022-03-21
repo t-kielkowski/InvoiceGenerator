e44e4688b03bf45ecb2e871d62e0e82d2fa16e62
t-kielkowski <tk.kielkowski@gmail.com>
2022-03-21 19:52:26 +0100

    Updated InvocieContex

--- InvoiceGenerator/Infrastructure/DbContex/InvocieContex.cs
+++ InvoiceGenerator/Infrastructure/DbContex/InvocieContex.cs
@@ -5,21 +5,17 @@ namespace InvoiceGenerator.InvoiceData
     public class InvocieContex : DbContext
     {
         public InvocieContex()
-        {
-                
-        }
-
+        { }
         public InvocieContex(DbContextOptions<InvocieContex> options) : base(options)
         { }
 
         public DbSet<Buyer> Buyers { get; set; }
         public DbSet<Seller> Seller { get; set; }
-        public DbSet<InvoiceDetails> InvoiceDetails { get; set; }
+        public DbSet<Invoice> InvoiceDetails { get; set; }
 
         //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         //{
         //    optionsBuilder.UseSqlServer("Data Source=PC-DOM;Initial Catalog=Invoices;Integrated Security=true");
         //}
-
     }
 }
