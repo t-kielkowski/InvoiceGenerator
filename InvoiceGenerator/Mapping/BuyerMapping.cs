b85ceb4b25cd26768159914ec5f102678fd1483e
Tomasz Kielkowski <71585480+t-kielkowski@users.noreply.github.com>
2022-04-13 18:15:13 +0200

    Added mapping

---
+++ InvoiceGenerator/Mapping/BuyerMapping.cs
@@ -0,0 +1,18 @@
+﻿using InvoiceGenerator.DTO;
+using InvoiceGenerator.InvoiceData;
+
+namespace InvoiceGenerator.Mapping
+{
+    internal static class BuyerMapping
+    {
+        public static BuyerDto Extend(this Buyer? buyer)
+        {
+            return new BuyerDto
+            {
+                Name = buyer.Name,
+                SecondName = buyer.SecondName,
+                FullDataString = $"{buyer.Name} \n {buyer.SecondName} \n {buyer.Street} \n {buyer.PostalCode} \n {buyer.City} \n"
+            };
+        }
+    }
+}
---
+++ InvoiceGenerator/Mapping/SellerMapping.cs
@@ -0,0 +1,18 @@
+﻿using InvoiceGenerator.DTO;
+using InvoiceGenerator.InvoiceData;
+
+namespace InvoiceGenerator.Mapping
+{
+    internal static class SellerMapping
+    {
+        public static SellerDto Extend(this Seller? seller)
+        {
+            return new SellerDto
+            {
+                Name = seller.Name,
+                SecondName = seller.SecondName,
+                FullDataString = $"{seller.Name} \n {seller.SecondName} \n {seller.Street} \n {seller.PostalCode} \n {seller.City} \n"
+            };
+        }
+    }
+}
