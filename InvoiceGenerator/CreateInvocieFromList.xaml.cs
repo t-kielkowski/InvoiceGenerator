9e6a216d697ed579791daac4bc14ac4205f24691
Tomasz Kielkowski <71585480+t-kielkowski@users.noreply.github.com>
2022-04-13 18:18:13 +0200

    Updated CreateInvocieFromList

--- InvoiceGenerator/CreateInvocieFromList.xaml.cs
+++ InvoiceGenerator/CreateInvocieFromList.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using InvoiceGenerator.Infrastructure.Repository.BuyerRepository;
 using InvoiceGenerator.Infrastructure.Repository.SellerRepository;
 using InvoiceGenerator.InvoiceData;
+using InvoiceGenerator.Mapping;
 
 namespace InvoiceGenerator
 {
@@ -16,9 +18,10 @@ namespace InvoiceGenerator
         private readonly IBuyerRepository _buyerRepository;
         private readonly ISellerRepository _sellerRepository;
 
-        public IEnumerable<Buyer> ComboBoxBuyerList { get; set; }
-        public IEnumerable<Seller> ComboBoxSellerList { get; set; }
-
+        public List<Buyer> BuyerList { get; set; }
+        public List<Seller> SellerList { get; set; }
+        public List<string> ComboBoxBuyerList { get; set; }
+        public List<string> ComboBoxSellerList { get; set; }
 
         public CreateInvocieFromList(IBuyerRepository buyerRepository, ISellerRepository sellerRepository)
         {
@@ -26,15 +29,38 @@ namespace InvoiceGenerator
 
             _buyerRepository = buyerRepository ?? throw new ArgumentNullException(nameof(buyerRepository));
             _sellerRepository = sellerRepository ?? throw new ArgumentNullException(nameof(sellerRepository));
-
-            LoadComboBoxData();
             DataContext = this;
+
+            LoadComboBox();
         }
 
-        private async void LoadComboBoxData()
+        private void LoadComboBox()
         {
-            ComboBoxBuyerList = await _buyerRepository.GetBuyerNameList();
-            ComboBoxSellerList = await _sellerRepository.GetSellerNameList();
+            BuyerList = _buyerRepository.GetAllAsync();
+            ComboBoxBuyerList = BuyerList.Select(x => $"{x.Name} {x.SecondName}").ToList();
+            ComBoxListOfBuyers.SelectedIndex = 0;
+
+            SellerList = _sellerRepository.GetAllAsync();
+            ComboBoxSellerList = SellerList.Select(x => $"{x.Name} {x.SecondName}").ToList();
+            ComBoxListOfSellers.SelectedIndex = 0;
+        }
+
+        private void ComBoxListOfBuyers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var currentItem = ComBoxListOfBuyers.SelectedItem.ToString();
+
+            var selectedItem = BuyerList
+                .FirstOrDefault(x => $"{x.Name} {x.SecondName}" == currentItem);
+            txtBlockBuyerData.Text = selectedItem.Extend().FullDataString;
+        }
+
+        private void ComBoxListOfSellers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var currentItem = ComBoxListOfSellers.SelectedItem.ToString();
+
+            var selectedItem = SellerList
+                .FirstOrDefault(x => $"{x.Name} {x.SecondName}" == currentItem);
+            txtBlockSellerData.Text = selectedItem.Extend().FullDataString;
         }
 
         private void btnMoveOn_Click(object sender, RoutedEventArgs e)
@@ -46,5 +72,7 @@ namespace InvoiceGenerator
         {
             this.Close();
         }
+
+       
     }
 }
